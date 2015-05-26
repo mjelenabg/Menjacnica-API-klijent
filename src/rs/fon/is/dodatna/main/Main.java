53013b94256edad6655cce4c4fa3f9891d3b8a9e
mjelenabg <mjelenabg@gmail.com>
2015-05-26 01:06:23 +0100

    Napravljena klasa Main sa main metodom.

---
+++ src/rs/fon/is/dodatna/main/Main.java
@@ -0,0 +1,13 @@
+package rs.fon.is.dodatna.main;
+
+import rs.fon.is.dodatna.azuriranje.AzuriranjeKursneListe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AzuriranjeKursneListe azuriranje = new AzuriranjeKursneListe();
+		azuriranje.azurirajValute();
+
+	}
+
+}
